961618cbb9c21506f9997c2b571074880b26eac6
Redryen <redryen.chula.ifpr.2023@gmail.com>
2025-10-15 14:55:23 -0300

    02

---
+++ DOM/js/ex00.js
@@ -0,0 +1,10 @@
+
+    function somar(){
+        var v1, v2, r
+        v1 =  document.getElementById("primeirovalor").value
+        v2 =  document.getElementById("segundovalor").value
+
+
+        
+    }
+
